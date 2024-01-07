25b46e8c28e21cfb46eee3d33954d6e9a3968242
Faizuddin Mohammed <115614604+F21Z@users.noreply.github.com>
2024-01-07 23:13:36 +0530

    slicing-solution

---
+++ 1-slicing.py
@@ -0,0 +1,8 @@
+#Creating the list
+mixed_data = [34, "Faizuddin", "Mohammed", 8.07, "MSS", 99.99, "Python Basics Evaluation", 7]
+
+#slicing
+
+print(mixed_data[:3])          #first three elements
+print(mixed_data[1:5])         #elements 2-5
+print(mixed_data[-2:])         #last two elements
