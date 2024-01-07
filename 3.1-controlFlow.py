3e3b1dc9a8edca4d07d98f8064ee1dfa43cc2979
Faizuddin Mohammed <115614604+F21Z@users.noreply.github.com>
2024-01-07 23:28:48 +0530

    Update-for-invalid-inputs

--- 3.1-controlFlow.py
+++ 3.1-controlFlow.py
@@ -3,15 +3,15 @@ def calculate_discount(original_price, discount_percentage=10):
     discounted_price = original_price*(1-(discount_percentage/100))            #calculating discount
     return discounted_price
 
-#testing from fixed input
-#print(calculate_discount(1950))
-#print(calculate_discount(1950,50))
 
 #from input
 while 1:
     price = input("Enter Original Price: $")
     if price=="":
         break                                                                  #exit loop if price not entered
+    if int(price) <= 0:
+        print("Invalid Input!!")
+        continue                                                               #prompt to re-enter if price is invalid(-ve or zero)
     disc_percent = input("Enter discount percentage: ")
     if disc_percent=="":
         disc_price = calculate_discount(int(price))                            #calling function using default discount of 10%
